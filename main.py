9f62e8bc2e1e83a8e6dd6c78c1f2b94fddadcd84
📊 OUSSAMA SEDDIK <140451607+Oussamaseddik@users.noreply.github.com>
2025-12-06 17:00:38 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# main_v1.py
+from src.Document import Document, RedditDocument, ArxivDocument
+from src.Corpus import Corpus
+
+
+corpus = Corpus("MonCorpus")
+
+
+doc1 = RedditDocument("Reddit Doc 1", "Alice", "2025-12-01", "url1", "Ceci est le texte du doc 1", comments=5)
+doc2 = ArxivDocument("Arxiv Doc 1", ["Bob", "Charlie"], "2025-11-20", "url2", "Ceci est le texte du doc 2")
+
+corpus.add_document(doc1)
+corpus.add_document(doc2)
+
+
+corpus.show_docs()
+
+
+corpus.save("data/corpus.csv")
+new_corpus = Corpus("CorpusChargé")
+new_corpus.load("data/corpus.csv")
+new_corpus.show_docs()
